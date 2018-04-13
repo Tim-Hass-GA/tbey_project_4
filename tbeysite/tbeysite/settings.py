48fc5fb5e693dc4441b4831977075a15bae4fce2
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-04-13 13:42:51 -0700

    for deployment

--- tbeysite/tbeysite/settings.py
+++ tbeysite/tbeysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'tbeystore/static/tbeystore/images/products/')
@@ -123,3 +123,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CART_SESSION_ID = 'cart'
+
+django_heroku.settings(locals())
